4712d449202e880c061b402144d811454fa9dde6
Antonio Leon <antonioleon@MacBook-Pro-de-Antonio-2.local>
2020-05-28 02:35:55 -0500

    ready to deploy

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // create an express app
 const express = require("express")
 const app = express()
-
+const path = require('path')
+var fs=require('fs');
 // use the express-static middleware
-app.use(express.static("public"))
+//app.use(express.static("public"))
+
+
+var aasa = fs.readFileSync('./public/apple-app-site-association');
+app.get('/apple-app-site-association', function(req, res, next) {
+    res.set('Content-Type', 'application/pkcs7-mime');
+    res.status(200).send(aasa);
+})
 
 // define the first route
 app.get("/", function (req, res) {
